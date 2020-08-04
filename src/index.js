f1897b3cdaa6b242f6b3df601a44eb2c6319f84e
krishna chandu akula <krishnachandu333@gmail.com>
2020-08-03 18:24:28 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import App from "./App";
+let greeting;
+
+ReactDOM.render(
+  <App />,
+  // <h1 className="heading" style={customStyle}>
+  document.getElementById("root")
+);
